8a911610a24879aadd81d7b940876dfeead8105a
Carlos Gonçalves <cgoncalves@av.it.pt>
2013-02-26 21:17:22 +0000

    Invoke the app deploy webservice on post receive

--- src/main/java/sonia/scm/plugins/csb/CsbDeployPostReceiveHook.java
+++ src/main/java/sonia/scm/plugins/csb/CsbDeployPostReceiveHook.java
@@ -1,5 +1,13 @@
 package sonia.scm.plugins.csb;
 
+import java.io.IOException;
+
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+
 import sonia.scm.plugin.ext.Extension;
 import sonia.scm.repository.PostReceiveRepositoryHook;
 import sonia.scm.repository.RepositoryHookEvent;
@@ -8,5 +16,17 @@ import sonia.scm.repository.RepositoryHookEvent;
 public class CsbDeployPostReceiveHook extends PostReceiveRepositoryHook {
 
 	public void onEvent(RepositoryHookEvent event) {
+		try {
+			String url = String.format("http://localhost:8080/csb/rest/%s/deploy", event.getRepository().getName());
+			DefaultHttpClient client = new DefaultHttpClient();
+			client.getCredentialsProvider().setCredentials(new AuthScope("localhost", 8080),
+					new UsernamePasswordCredentials("portal", "qwerty321"));
+			HttpPost post = new HttpPost(url);
+			client.execute(post);
+		} catch (ClientProtocolException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
